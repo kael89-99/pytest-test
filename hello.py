2bbff3d1654ba24a4834bdedb4472b4b7c2ffd50
kael89-99 <131792774+kael89-99@users.noreply.github.com>
2025-11-20 04:30:59 +0000

    adding initial layout of python project

---
+++ hello.py
@@ -0,0 +1,16 @@
+def more_hello():
+    return "hi"
+
+
+def more_goodbye():
+    return "bye"
+
+
+# this is nonesense code to chatch a warning
+# var =  1
+# var = var
+
+
+# bad syntax
+
+# foo =
